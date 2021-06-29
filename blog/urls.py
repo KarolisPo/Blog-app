5c72174b15bc5728aafe513ed66fdfd7181c1b21
Carl <karo.pociu@gmail.com>
2021-06-29 15:19:33 +0300

    forms and user accounts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import BlogDetailView, BlogListView
+from .views import BlodDeleteView, BlogCreateView, BlogDetailView, BlogListView, BlogUpdateView
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail')
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete', BlodDeleteView.as_view(), name='post_delete'),
+    
 ]
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))] 
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
